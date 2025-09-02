1a6a57eb7c3a1878a88fe369f65f0e2fa0772207
Carlos Digaetano <digaetano.dev@gmail.com>
2025-09-02 15:33:39 -0400

    Created Area of Circle Calculation

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Concurrent;
+
+namespace ConsoleApp;
+
+class Program
+{
+    public static double CalculateAreaCircle (double radius)
+    {
+        return Math.PI * radius * radius;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the radius of the circle:");
+        double radius = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine($"The area is {CalculateAreaCircle(radius)}");
+        
+    }
+
+}
